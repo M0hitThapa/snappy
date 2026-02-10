98aea694de15c7f77ba7d88f125ab00fd8ee08f7
Mohit Thapa <thapamohit2058@gmail.com>
2026-02-10 22:22:34 +0545

    made all schemas and their relation

--- server/db/schemas/posts.ts
+++ server/db/schemas/posts.ts
@@ -1,7 +1,8 @@
 import { relations } from "drizzle-orm";
 import { integer, pgTable, serial, text, timestamp } from "drizzle-orm/pg-core";
 import { userTable } from "./auth";
-import { commentUpvotesTable, postUpvotesTable } from "./upvotes";
+import {  postUpvotesTable } from "./upvotes";
+import { commentsTable } from "./comments";
 
 export const postsTable = pgTable("posts", {
     id:serial("id").primaryKey(),
@@ -27,5 +28,5 @@ export const postRelations = relations(postsTable, ({one,many}) =>({
     postUpvotesTable:many(postUpvotesTable, {
         relationName:"postUpvotes"
     }),
-    comments:many(commentUpvotesTable)
+    comments:many(commentsTable)
 }))
--- server/db/schemas/upvotes.ts
+++ server/db/schemas/upvotes.ts
@@ -1,4 +1,8 @@
+import { relations } from "drizzle-orm";
 import { integer, pgTable, serial, text, timestamp } from "drizzle-orm/pg-core";
+import { postsTable } from "./posts";
+import { userTable } from "./auth";
+import { commentsTable } from "./comments";
 
 export const postUpvotesTable = pgTable("postupvotestable", {
     id:serial("id").primaryKey(),
@@ -8,6 +12,19 @@ export const postUpvotesTable = pgTable("postupvotestable", {
 })
 
 
+export const postUpvotesRelation = relations(postUpvotesTable, ({one}) => ({
+post:one(postsTable, {
+    fields:[postUpvotesTable.postId],
+    references:[postsTable.id],
+    relationName:"postUpvotes"
+}),
+user:one(userTable,{
+    fields:[postUpvotesTable.userId],
+    references:[userTable.id],
+    relationName:"user"
+} ) 
+}));
+
 
 
 export const commentUpvotesTable = pgTable("commentupvotetable", {
@@ -16,3 +33,16 @@ export const commentUpvotesTable = pgTable("commentupvotetable", {
     commentId:integer("comment_id").notNull(),
     createdAt:timestamp("created_at", {withTimezone:true}).defaultNow().notNull()
 })
+
+export const commentUpvoteRelation = relations(commentUpvotesTable, ({one}) => ({
+    post:one(commentsTable, {
+        fields:[commentUpvotesTable.commentId],
+        references:[commentsTable.id],
+        relationName:"commentUpvotes"
+    }),
+    user:one(userTable, {
+        fields:[commentUpvotesTable.userId],
+        references:[userTable.id],
+        relationName:"user"
+    })
+}))

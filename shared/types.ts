f93d059e4c95c1fa63225e23233d2fe8dd884a5f
Mohit Thapa <thapamohit2058@gmail.com>
2026-02-17 18:00:32 +0545

    add pagination schema

--- shared/types.ts
+++ shared/types.ts
@@ -1,3 +1,4 @@
+import { insertPostSchema } from "@/db/schemas/posts";
 import z from "zod";
 
 export type SuccessMessage<T = void> = {
@@ -18,4 +19,30 @@ export const LoginSchema = z.object({
     .max(31)
     .regex(/^[a-zA-Z0-9_]+$/),
   password: z.string().min(3).max(255),
+});
+
+export const createPostSchema = insertPostSchema
+  .pick({
+    title: true,
+    url: true,
+    content: true,
+  })
+  .refine((data) => data.url || data.content, {
+    message: "either url of content must be provided",
+    path: ["url", "content"],
+  });
+
+export const sortBySchema = z.enum(["points", "recent"]);
+export const orderSchema = z.enum(["asc", "desc"]);
+
+export type sortBy = z.infer<typeof sortBySchema>;
+export type order = z.infer<typeof orderSchema>;
+
+export const paginationSchema = z.object({
+  page: z.coerce.number().optional().default(10),
+  limit: z.coerce.number().optional().default(1),
+  sortBy: sortBySchema.optional().default("points"),
+  order: orderSchema.optional().default("desc"),
+  author: z.optional(z.string()),
+  site: z.string().optional(),
 });
